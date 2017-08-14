99983b921578991a4f7d29f465a11ce2d1d64592
Rastislav Ondrasek <ondraser@amazon.com>
2017-08-13 19:27:19 -0700

    Adding class-based search bar

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBVCXcDZJ-HSIs8iWD1c2LelpvbvO-I7ak';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
